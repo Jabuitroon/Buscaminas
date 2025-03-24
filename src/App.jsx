eb0d3ff1160e8cfb1673f297693b5370d778bd96
Jabuitroon <Jandresbuiitron@gmail.com>
2025-03-23 21:41:34 -0500

    Dist creado

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { MinesweeperBanner } from './components/UI/BuscaMinasBanner'
 import { GameTable } from './components/game-table/GameTable'
 
